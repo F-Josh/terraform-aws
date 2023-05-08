bc9fe83ad9d0708f8e1207f5b051f76b3f2cad69
Josh <josh.nnadozie@gmail.com>
2023-05-08 13:02:18 -0400

    creatd an ec2 instance and deployed 2048 to run on apache2

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,13 @@
 sudo apt-get update
 
 # Install the Apache HTTP Server
-sudo apt-get install -y apache2 
+sudo apt-get install -y apache2 git
+
+#Download the source code from git
+git clone https://github.com/gabrielecirulli/2048.git
+
+#put the spurce code into the webserver root directory
+sudo cp -R 2048/* /var/www/html/  ## this is the weberver root for apache
 
 # Start the Apache service
 sudo systemctl start apache2
