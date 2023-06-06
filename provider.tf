9ce54c19b75f884d6dddd1486a43a81391f8154c
Josh <josh.nnadozie@gmail.com>
2023-06-05 21:03:11 -0400

    Configured the remote state by manually settiny up s3 and dynamodb

---
+++ provider.tf
@@ -0,0 +1,16 @@
+### provider block......
+provider "aws" {
+  region  = "us-east-1"
+  profile = "josh"
+}
+
+terraform {
+  backend "s3" {
+  bucket = "terraform-remote-state-vlad-mentorship"
+  key    = "terraform.tfstate"
+  dynamodb_table = "terraform-remote-state-vlad-mentorship"
+  region = "us-east-1"
+  profile = "josh"
+}
+
+}
